56ffbc35670c55de9dc6cd81a0f820e95d71d577
Marco Righi <marco.righi@tasgroup.eu>
2021-05-03 14:37:07 +0200

    Initial commit

---
+++ src/test/java/org/micro/NativeEntryPointResourceIT.java
@@ -0,0 +1,9 @@
+package org.micro;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeEntryPointResourceIT extends EntryPointResourceTest {
+
+    // Execute the same tests but in native mode.
+}
